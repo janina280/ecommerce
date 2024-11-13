dcb6a5b0452bdfb89d9b34ddb9ff58dbcc9ea61b
janina280 <coceijanina@yahoo.com>
2024-11-13 23:30:14 +0200

    rename

--- Server/src/main/java/com/ecommerce/topit/TopItApplication.java
+++ Server/src/main/java/com/ecommerce/topit/TopItApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShopEaseApplication {
+public class TopItApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShopEaseApplication.class, args);
+		SpringApplication.run(TopItApplication.class, args);
 	}
 
 }
