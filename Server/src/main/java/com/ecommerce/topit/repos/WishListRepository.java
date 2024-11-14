efcb3629b641669f0dc856d9284ff5a95425995a
janina280 <coceijanina@yahoo.com>
2024-11-14 23:22:40 +0200

    styling

---
+++ Server/src/main/java/com/ecommerce/topit/repos/WishListRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.topit.repos;
+
+import com.ecommerce.topit.models.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/*@Repository
+public interface WishListRepository extends JpaRepository<CartItem, Long> {
+}*/
